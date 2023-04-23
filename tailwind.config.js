46c2e7b3a5436fc126fa072c7531c8c9b16cb67c
Paul Zhang <paulz@ucla.edu>
2023-04-23 15:10:21 -0700

    Use prototype ieeebruins theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	daisyui: {
+		themes: [
+			{
+				ieeebruins: {
+					primary: '#0091E6',
+					secondary: '#F144C0',
+					accent: '#00D595',
+					neutral: '#3D4451',
+					'base-100': '#FFFFFF',
+					info: '#3ABFF8',
+					success: '#36D399',
+					warning: '#FBBD23',
+					error: '#F87272'
+				}
+			}
+		]
+	},
 	theme: {
 		extend: {}
 	},
