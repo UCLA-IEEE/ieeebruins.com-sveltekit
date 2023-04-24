eb66b509e046e6e0b98c87c43d82056f2ffc1f6e
Paul Zhang <paulz@ucla.edu>
2023-04-23 18:45:49 -0700

    Add prototype navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,44 @@
-<script>
-  import "../app.css";
+<script lang="ts">
+	import '../app.css';
+
+	import NavList from './NavList.svelte';
+	import { Menu, ChevronDown, ChevronRight } from 'lucide-svelte';
 </script>
 
-<slot />
+<div class="drawer">
+	<input id="navbar-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Desktop navbar -->
+		<div class="navbar outline outline-primary">
+			<!-- IEEE at UCLA icon -->
+			<div class="navbar-start">
+				<a href="/" class="btn btn-ghost normal-case text-2xl p-2">IEEE at UCLA</a>
+			</div>
+
+			<!-- Desktop navigation list -->
+			<div class="navbar-end hidden lg:flex">
+				<ul class="menu menu-horizontal bg-base-100 p-2">
+					<NavList dropdownIcon={ChevronDown} />
+				</ul>
+			</div>
+
+			<!-- Menu button to toggle sidebar, only shown on mobile -->
+			<div class="navbar-end lg:hidden">
+				<label for="navbar-drawer" class="btn btn-square btn-ghost">
+					<Menu />
+				</label>
+			</div>
+		</div>
+
+		<!-- Page content -->
+		<slot />
+	</div>
+
+	<!-- Mobile sidebar -->
+	<div class="drawer-side">
+		<label for="navbar-drawer" class="drawer-overlay" />
+		<ul class="menu bg-base-100 p-2 w-1/4">
+			<NavList dropdownIcon={ChevronRight} />
+		</ul>
+	</div>
+</div>
---
+++ src/routes/NavList.svelte
@@ -0,0 +1,25 @@
+<!--
+  @component
+  List of navigation links
+ -->
+
+<script lang="ts">
+	import type { ComponentType } from 'svelte';
+
+	export let dropdownIcon: ComponentType;
+</script>
+
+<li>
+	<a href="/about">
+		About
+		<svelte:component this={dropdownIcon} />
+	</a>
+	<ul class="bg-base-100 p-2 shadow">
+		<li><a href="/officers">Officers</a></li>
+		<li><a href="/edi">EDI</a></li>
+	</ul>
+</li>
+<li><a href="/projects">Projects</a></li>
+<li><a href="/workshops">Workshops</a></li>
+<li><a href="/lab">Lab</a></li>
+<li><a href="/sponsors">Sponsors</a></li>
