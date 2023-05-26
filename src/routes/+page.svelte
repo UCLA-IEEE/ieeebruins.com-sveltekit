250a38381ec86fdde9f54a4e6fb0860ffa6cf17b
Paul Zhang <paulz@ucla.edu>
2023-05-25 18:52:05 -0700

    Add About Us and Get Involved sections to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
 <script>
+	import CtaButton from './CtaButton.svelte';
+
 	import ops from '$lib/images/ops.jpg';
-	import IeeeLogo from '../lib/components/IeeeLogo.svelte';
+	import IeeeLogo from '$lib/components/IeeeLogo.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import SimpleIcon from '$lib/components/SimpleIcon.svelte';
 </script>
 
 <div class="hero min-h-screen" style="background-image: url({ops})">
 	<div class="hero-overlay bg-opacity-50" />
 	<div class="hero-content text-center">
 		<div class="max-w-max">
-			<h1 class="flex gap-8 text-9xl text-base-100 font-black m-10 bg-primary bg-opacity-80 p-2">
-				<IeeeLogo color="#ffffff" className="w-32 h-32" />
+			<h1
+				class="flex items-center glass gap-8 text-5xl lg:text-9xl text-base-100 font-black m-10 bg-primary bg-opacity-80 p-2"
+			>
+				<IeeeLogo color="#ffffff" className="w-28 h-28" />
 				<span> IEEE at UCLA </span>
 			</h1>
 			<h2 class="text-5xl text-base-100 font-bold">
@@ -18,15 +24,59 @@
 	</div>
 </div>
 
-<div class="text-9xl">more stuff lol</div>
+<div class="flex flex-col lg:flex-row self-center max-w-screen-lg my-8 mx-4 gap-4">
+	<div class="lg:w-1/2">
+		<Header text="About Us" />
+		<p class="mb-2">
+			IEEE at UCLA is a professional student-run engineering organization. We devote our time and
+			energy towards bringing hands-on, practical experiences to engineering students throughout
+			UCLA, as well as create opportunities for students to interact with industry professionals and
+			more.
+		</p>
+		<p>
+			Every year we lead several projects for our members to participate in, where dedicated leads
+			provide fundamental knowledge and skills to students, which helps them complete challenging
+			and interesting projects throughout the year.
+		</p>
+	</div>
+	<div class="lg:divider lg:divider-horizontal" />
+	<div class="lg:w-1/2">
+		<Header text="Get Involved" />
+		<p class="text-xl mb-2">Keep up with all our upcoming events, workshops, and socials.</p>
+		<div class="flex flex-col self-center gap-4">
+			<CtaButton
+				href="https://discord.gg/RREtsea"
+				iconSlug="discord"
+				color="#5865F2"
+				text="Join our Discord"
+			/>
+			<CtaButton
+				href="https://uclaieeenewsletter.substack.com/"
+				iconSlug="substack"
+				color="#FF6719"
+				text="Subscribe to our newsletter"
+			/>
+			<CtaButton
+				href="https://www.instagram.com/uclaieee/"
+				iconSlug="instagram"
+				color="#E4405F"
+				text="Follow us on Instagram"
+			/>
+		</div>
+	</div>
+</div>
+
+<div class="flex self-center w-full max-w-screen-lg my-8 mx-4">
+	<Header text="Events" />
+</div>
 
 <style>
-	#based-logo {
+	/* #based-logo {
 		background: linear-gradient(-45deg, hsl(var(--p)), hsl(var(--s)), hsl(var(--a)), #23d5ab);
 		background-clip: content-box;
 		background-size: 400% 400%;
 		animation: gradient 15s ease infinite;
-	}
+	} */
 
 	@keyframes gradient {
 		0% {
---
+++ src/routes/CtaButton.svelte
@@ -0,0 +1,23 @@
+<!-- @component
+Call to action button
+ -->
+
+<script lang="ts">
+	import SimpleIcon from '$lib/components/SimpleIcon.svelte';
+
+	export let href: string;
+	export let color: string;
+	export let iconSlug: string;
+	export let text: string;
+</script>
+
+<a
+	role="button"
+	{href}
+	class={`btn btn-primary border-[${color}] bg-[${color}] gap-2 max-w-fit`}
+	target="_blank"
+	rel="noopener noreferrer"
+>
+	<SimpleIcon {iconSlug} size={24} />
+	{text}
+</a>
