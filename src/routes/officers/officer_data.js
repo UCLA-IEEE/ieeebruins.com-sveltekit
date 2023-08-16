d0e0bd891031464a794b30171a56d0a85d9cd0fe
JonathonYY <43181115+JonathonYY@users.noreply.github.com>
2023-08-15 21:55:08 -0700

    Add officer data

---
+++ src/routes/officers/officer_data.js
@@ -0,0 +1,286 @@
+export const officer_boards = [
+    {
+        name: "Executive Board",
+        officers: [
+            {
+                name: "Achinthya Poduval",
+                position: "President",
+                major: "EE",
+                class: "4th Year",
+                picture: "$lib/images/" +  "achinthya.jpg",
+                description: "What’s up y’all? Technically, I’m a 4th year EE, I organize stuff Presidentially, and I’ve been in IEEE long enough to have done most of the projects. But really, I just captain the IM FrisbIEEE team. If you want to learn anything IEEE related or just have questions for me, feel free to ask!<br><br>And more importantly, if you want to play frisbee anytime, DEFINITELY ask me ;).",
+                pronouns: "He/Him"
+            },
+
+            {
+                name: "Esha Thota",
+                position: "Internal Vice President",
+                major: "CE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "esha.jpg",
+                description: "Hello! My name is Esha Thota, and as the Internal Vice President for IEEE, I organize and manage events for IEEE, including socials, town halls, and more! You can always reach out to ask me about undergraduate research, internships, CE, IEEE, and anything else you are curious about!",
+                pronouns: "She/Her"
+            },
+            
+            {
+                name: "Bradley Schulz",
+                position: "External Vice President",
+                major: "CE",
+                class: "4th Year",
+                picture: "$lib/images/" +  "bradley.jpg",
+                description: "Hi! I'm Bradley, and I'm a 4th year computer engineering with a minor in environmental systems and societies! I am currently external vice president, and in past years I was Micromouse lead and Project and Lab Manager. Feel free to reach about to ask about internships, sustainability stuff, running (I love to run), or just life in general!",
+                pronouns: "He/Him"
+            },
+
+            {
+                name: "Jason Song",
+                position: "External Vice President",
+                major: "EE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "jason.jpg",
+                description: "Hi! My name is Jason Song, and I am External Vice President of IEEE. I am responsible for organizing the IDEA Hacks hardware hackathon, graduate school info sessions, and the Student Professional Awareness Venture. I love boxing, hip hop, RnB, dance music, airplanes, and cars. Please feel free to reach out to me if you have any questions about classes, IEEE or UCLA!",
+                pronouns: "He/Him"
+            }
+        ]
+    },
+
+    {
+        name: "Admin Board",
+        officers: [
+            {
+                name: "Taylor Chung",
+                position: "Treasurer",
+                major: "CS",
+                class: "4th Year",
+                picture: "$lib/images/" +  "taylor_c.jpg",
+                description: "Hi there, my name is Taylor and I'm this year's treasurer! I'm responsible for anything money-related in IEEE, feel free to direct DM me any questions and I'll help you out to the best of my abilities. When I'm not looking at spreadsheets and checking things off, you'll find me at the gym, climbing synthetic rocks, roasting & getting roasted by my friends, and/or going to target for no reason. Say hi if you see me around!",
+                pronouns: "She/Her"
+            },
+
+            {
+                name: "Taylor Kawate",
+                position: "Corporate Relations",
+                major: "EE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "taylor_k.jpg",
+                description: "Hello! My name is Taylor Kawate and I am the Corporate Relations Officer for IEEE at UCLA. My main responsibilities include organizing corporate information sessions, company recruiting events, and networking with company representatives to bring you exciting opportunities. Feel free to say hello if you see me around, I’d love to talk about anything from classes, internships, and projects to music, working out, and gaming (especially Minecraft)! See you around :)",
+                pronouns: "He/Him"
+            },
+
+            {
+                name: "Theo Zhang",
+                position: "Secretary & EDI Chair",
+                major: "CS",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "theo.jpg",
+                description: "I'm Theo, the Secretary and Equity, Diversity, and Inclusion Chair for UCLA IEEE! I lead the Alumni Mentorship Program and EDI efforts to create networking and mentorship opportunities as well as make our club inclusive and welcoming! I am a staff writer for a satirical newsletter, as well as an avid reader and okay cook (although I'm really good at making handmade noodles). Don't be afraid to reach out and say hi!",
+                pronouns: "They/Them"
+            },
+            
+            {
+                name: "Claire Huang",
+                position: "Publicity",
+                major: "CSE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "claire.jpg",
+                description: "Hello! My name is Claire, and I'm your publicity officer! I'm the one behind IEEE's social media, and I also design and order merchandise, make the posters, and take pictures of club events. Feel free to get in touch if you want to chat about anything!",
+                pronouns: "She/Her"
+            },
+
+            {
+                name: "Jacqueline Duong",
+                position: "Events Coordinator",
+                major: "EE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "jacqueline.jpg",
+                description: "What's up! My name is Jacqueline, a 2nd year EE and one of the events coords for this year. I book rooms and plan events, plus am responsible for all the good food you'll eat at IEEE events this year (so be good to me >:))! I am always free to go on a boba run if you're down #no sugar less ice gang so feel free to hmu :) Hope to meet everyone at all the events this year! Per usual, be there or be circled :)",
+                pronouns: "She/Her"
+            },
+
+            {
+                name: "Vidhya Prabhu",
+                position: "Events Coordinator",
+                major: "EE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "vidhya.jpg",
+                description: "Hi! I'm Vidhya, one of your events coordinators this year. I handle event logistics and help plan socials along with my co-chair, Jackie (come see the two of us in the lab if you wanna hear some knee-slappers). I enjoy all kinds of music, going to concerts, and writing. I'm also in DAV this year :)",
+                pronouns: "She/Her"
+            },
+
+            {
+                name: "Ryan Tien",
+                position: "Outreach Coordinator",
+                major: "EE",
+                class: "4th Year",
+                picture: "$lib/images/" +  "ryan.jpg",
+                description: "Hey everyone! My name is Ryan, and I'm this year's Outreach Coordinator. I lead the Outreach Committee, in which together we host technical workshops and participate in science fairs and exhibits around our local community to help inspire the future generation of engineers. I also run our OPS2.0 program. In my free time, you can catch me watching a Marvel movie/TV show, playing basketball, or exploring the city with friends. Please feel free to reach out if you any questions about engineering, sports, the MCU, or college life in general!",
+                pronouns: "He/Him"
+            }
+        ]
+    },
+    
+    {
+        name: "Projects Board",
+        officers: [
+            {
+                name: "Eli Foerst",
+                position: "OPS Lead",
+                major: "EE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "eli.jpg",
+                description: "Hello my name is Eli, as your OPS Co-Lead I focus on helping you learn more about the basics and coordinate our <i>new</i> WorkshOPS for this year. I'm stoked to meet anyone new so feel free to reach out to me if you need advice, resume critiques, or just want to go to the beach.",
+                pronouns: "He/Him"
+            },
+                
+            {
+                name: "Miriam Nygren",
+                position: "OPS Lead",
+                major: "EE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "miriam.jpg",
+                description: "Hi, I'm Miriam, one of your two friendly neighborhood OPS leads! I work with Eli on organizing lectures, projects, worksessions, and everything else OPS-related. Some things I love are ice cream, YouTube, and helping YOU learn about electrical engineering. If you ever want to chat about your projects, school, or niche media & documentaries, send me a message!",
+                pronouns: "She/Her"
+            },
+
+            {
+                name: "Megan Chen",
+                position: "Micromouse Lead",
+                major: "EE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "megan.jpg",
+                description: "Hi everyone! I'm Megan Chen, a second year EE major, and one of your Micro-Leads this year. I'm excited to lead Micromouse this year, and can't wait to work with y'all! I'm frequently just vibing in the lab so feel free to hit me up and chat with me about anything! ",
+                pronouns: "Any Pronouns"
+            },
+            
+            {
+                name: "Nathan Nguyendinh",
+                position: "Micromouse Lead",
+                major: "CE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "nathan_n.jpg",
+                description: "YOOOOOOOOOOOOOOOOOOOOOOOOOOO! What's up I'm Nathan and I'm one of this year's Micro-leads. If you're rly curious and nosy enough to actually read through these, I may as well tell you that I'm a second year CE major, I stand at a menacing 5'4&quot;, I'm a music theory nerd, I'm a Marylander that snuck his way into getting in-state tuition, and my Social Security number is [REDACTED]. If you're ever in the IEEE lab and see a short asian male with circle glasses, feel free to say hi! (and if you're lucky, the person you say hi to might even be me)",
+                pronouns: "He/Him",
+            },
+
+            {
+                name: "Cameron Fiske",
+                position: "Pocket Racers Lead",
+                major: "CE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "cameron.jpg",
+                description: "What up y'all, I'm Cameron. I'm a CE major (that's Cameron Engineering, of course). When I'm not eating crayons, I like to go running and hunt whales, sometimes at the same time. They call me Camera N. because I'm always watching you... I can even see that you're reading the IEEE website right now. Anyway, if you ever wanna chat, feel free to hit me up :-)",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Premkumar Giridhar",
+                position: "Pocket Racers Lead",
+                major: "CS",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "prem.jpg",
+                description: "Hello, my name is Prem! After my unfortunate incident in Willy Wonka's TV room, I decided to commit myself to CS in order to make sure no other young children will have to go through the taffy stretcher. This year, I am leading our brand new Pocket Racers project, so feel free to ask me about IEEE! ",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Timothy Jacques",
+                position: "DAV Lead",
+                major: "CE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "tim.jpg",
+                description: "Hello! I am one of this year's DAV Leads, and am a former MM lead. I'm a second year computer engineer, and love anything embedded or digital design related. My hobbies include biking, hiking, making, and gaming. Feel free to ask about anything IEEE related, or just for advice in general!",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Siddhant Gupta",
+                position: "DAV Lead",
+                major: "CE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "siddhant.jpg",
+                description: "Hey y'all, I'm Siddhant and I'm a third-year CE from the Bay Area! I was Workshops Manager last year and I'm excited to be leading DAV this year since I love chip design and computers in general. Outside of IEEE I'm a big fan of spending time in nature (hiking, camping, biking, swimming etc) but I have been known to partake in moderate videogaming as well. Feel free to reach out about any of that (or anything else) :)",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Nathan Pereira",
+                position: "WRAP Lead",
+                major: "EE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "nathan_p.jpg",
+                description: "Hi, my name is Nathan. As WRAP co-lead, I’m looking forward to teaching y’all how to communicate with each other (wirelessly). Ask me anything about UCLA, classes, or really anything. Look forward to working with everyone this year!",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Ramtin Sharafoleslami",
+                position: "WRAP Lead",
+                major: "EE",
+                class: "4th Year",
+                picture: "$lib/images/" +  "ramtin.jpg",
+                description: "Hi! I'm Ramtin, one of the WRAP leads this year. I mainly focus on the RF hardware side of the project because I like circuits in general. Beside EE, I'm always down to talk about art, philosophy, and social topics as well. Hope to see you in the lab!",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Caden Davis",
+                position: "Workshops Manager",
+                major: "EE",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "caden.jpg",
+                description: "Hey everyone, I'm Caden! I'm a second year EE major and one of IEEE's Workshops Managers this year. I can't wait to wow you all with some exciting workshops this year, but outside of those, feel free to reach out to me! Whether about classes, clubs, undergraduate research, IEEE, workshop suggestions, paddleboarding, legos, photography, etc., just hit me up :D",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Jessica Chang",
+                position: "Workshops Manager",
+                major: "EE",
+                class: "4th Year",
+                picture: "$lib/images/" +  "jessica.jpg",
+                description: "Hello I’m Jessica! I am a second year transfer, and I will be one of your Workshops Managers this year. Our workshops are tailored to be the foundation block for your future endeavors. We hope you can take these blocks and build amazing IEEE projects or self-projects. Feel free to reach out to me if you want to talk about transitioning from CC to UCLA, finding research labs, cooking, cute cats, or life in general. ;D So excited to meet you all!",
+                pronouns: "She/Her"
+            },
+            
+            {
+                name: "Andrew Fantino",
+                position: "Projects and Lab Manager",
+                major: "CE",
+                class: "4th Year",
+                picture: "$lib/images/" +  "andrew.jpg",
+                description: "Hi everyone, I'm Andrew! I am one of this year's two Project and Lab Managers. I work with Dominic with everything PLM related. I would be happy to talk to you about any projects you have in mind, changing majors, climbing, surfing, and lifting. I am always in the lab and will usually be able to help with some homework questions.",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Dominic Olson",
+                position: "Projects and Lab Manager",
+                major: "EE",
+                class: "3rd Year",
+                picture: "$lib/images/" +  "dominic.jpg",
+                description: "Hello, I'm Dominic! I'm one of the Projects and Lab Managers this year, where I oversee IEEE's technical pursuits and the lab space. In other words, I'm here to make sure you learn a lot and have a lot of fun as an IEEE member. Feel free to say hi in the lab or reach out if you want to chat about engineering projects, classes at UCLA, music, or anything else!",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Tyler Price",
+                position: "Research and Development",
+                major: "EE",
+                class: "4th Year",
+                picture: "$lib/images/" +  "tyler.jpg",
+                description: "Hello there :D I am IEEE's R&D lead, which means I oversee the SPI program. Feel free to reach out if you would like to chat about anything, and feel obligated to reach out if you would like to chat about sumo or Weezer.",
+                pronouns: "He/Him"
+            },
+            
+            {
+                name: "Paul Zhang",
+                position: "Webmaster",
+                major: "CS",
+                class: "2nd Year",
+                picture: "$lib/images/" +  "paul.jpg",
+                description: "(˘ ε˘ʃƪ) Hewwo I'm Paul, the webmaster for IEEE. I'm a second year and a pure CS major, untainted by the hardware vagaries of EE. Just kidding tho, I love being part of IEEE even though I cannot do hardware to save my life. :3<br><br>I love reading books, so please hit me up if you have recommendations! Always looking to expand my reading list.<br><br>Always feel free to say hi, I love meeting new people! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
+                pronouns: "He/Him"
+            }
+        ]
+    }
+];
