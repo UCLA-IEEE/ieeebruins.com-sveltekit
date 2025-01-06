23f9546ba972ca51aa0fa9546e2110cf9322780e
KevZ1209 <kevinbowditch11@gmail.com>
2025-01-05 18:26:23 -0800

    update lab hours spreadsheet to winter 2025

--- src/routes/api/lab-hours/+server.ts
+++ src/routes/api/lab-hours/+server.ts
@@ -3,7 +3,7 @@ import { API_KEY } from '$env/static/private';
 
 const SPREADSHEET_ID = '1xI1JU96y4FPuAjm5aXoc5iQoQ0If2HZ6yeUI_LBhL0E';
 const API_BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets/';
-const HOURS_SHEET = 'Fall%202024'; // Note that %20 is the URL encoding for a space (" ")
+const HOURS_SHEET = 'Winter%202025'; // Note that %20 is the URL encoding for a space (" ")
 const HOURS_OPTIONS = '/values/' + HOURS_SHEET + '!B3:G10?key=';
 const SPECIAL_TAIL = '%20-%20Special%20Hours%20(for%20Bot%20purposes)';
 const SPECIAL_SHEET = HOURS_SHEET + SPECIAL_TAIL;
