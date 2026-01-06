71018ae3114f79ccc0263340624cf18b230dcdd5
Zach Fischer <zfischer42@gmail.com>
2026-01-06 12:09:34 -0800

    Update lab hours from Fall to Winter

--- src/routes/api/lab-hours/+server.ts
+++ src/routes/api/lab-hours/+server.ts
@@ -3,7 +3,7 @@ import { API_KEY } from '$env/static/private';
 
 const SPREADSHEET_ID = '1esZ7bwd42nXYLt3g213FOMRtR1SbpdZyza3xhSwPAcg';
 const API_BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets/';
-const HOURS_SHEET = 'Fall%202026'; // Note that %20 is the URL encoding for a space (" ")
+const HOURS_SHEET = 'Winter%202026'; // Note that %20 is the URL encoding for a space (" ")
 const HOURS_OPTIONS = '/values/' + HOURS_SHEET + '!B3:G10?key=';
 const SPECIAL_TAIL = '%20-%20Special%20Hours%20(for%20Bot%20purposes)';
 const SPECIAL_SHEET = HOURS_SHEET + SPECIAL_TAIL;
